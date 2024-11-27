f706064c5c1ad0bc021429f095c14fa5bce01340
FireEcho <h42gen@gmail.com>
2024-11-27 20:20:57 +0100

    copied from task

--- praktikum/02/Looping.java
+++ praktikum/02/Looping.java
@@ -1,3 +1,14 @@
 public class Looping{
-    
+    public static void main(String[] s) {
+        /*long N = …; // Werte, die zum Ueberlauf fuehren oder nicht
+        // Ihr Kernalgorithmus …
+        long R = … // der von Ihnen bestimmte ‚umgedrehte‘ Zahlenwert
+        // System.out.println("R = " + R); // wird nicht mehr gebraucht, kann
+        // aber als Kommentar im Programm verbleiben und ggf. aktiviert werden
+        
+        if (<𝑁+𝑅 könnte zu einem Überlauf führen>)
+            System.out.println("Ueberlauf"); 
+        else 
+            System.out.println(N+R); // korrekt, ohne Ueberlauf */
+    }
 }
