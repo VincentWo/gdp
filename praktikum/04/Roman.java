535c81c0a33f778e6bdb4340621df3ea1bed4720
FireEcho <h42gen@gmail.com>
2025-01-12 13:50:36 +0100

    Subtraktionsregel und assert fehlen

--- praktikum/04/Roman.java
+++ praktikum/04/Roman.java
@@ -1,9 +1,33 @@
 public class Roman {
     static String roman(int n) { 
-        /* TODO */ 
+        if(n>=1000){
+            return "M" + roman(n-1000);
+        }
+        else if(n>=500){
+            return "D" + roman(n-500);
+        }
+        else if(n>=100){
+            return "C" + roman(n-100);
+        }
+        else if(n>=50){
+            return "L" + roman(n-50);
+        }
+        else if(n>=10){
+            return "X" + roman(n-10);
+        }
+        else if(n>=5){
+            return "V" + roman(n-5);
+        }
+        else if(n>=1){
+            return "I" + roman(n-1);
+        }
+        else {
+            return "";
+        }
     }
     public static void main(String[] args) {
         // Behandlung fehlender oder falscher Eingabeparameter
+        int N = Integer.parseInt(args[0]);
         assert(1 <= N && N <= 5000);
         System.out.println(roman(N));
     }
