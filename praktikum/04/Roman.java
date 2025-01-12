67078a4dfc2dca1f3ef53291cc972219259af68c
FireEcho <h42gen@gmail.com>
2025-01-12 13:37:06 +0100

    Roman.java erstellt

---
+++ praktikum/04/Roman.java
@@ -0,0 +1,10 @@
+public class Roman {
+    static String roman(int n) { 
+        /* TODO */ 
+    }
+    public static void main(String[] args) {
+        // Behandlung fehlender oder falscher Eingabeparameter
+        assert(1 <= N && N <= 5000);
+        System.out.println(roman(N));
+    }
+}
