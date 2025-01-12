4ed77374744a74b256b07ece87310ba9c32acb67
FireEcho <h42gen@gmail.com>
2025-01-12 18:03:32 +0100

    Roman in deutlich huebscher neugemacht

--- praktikum/04/Roman.java
+++ praktikum/04/Roman.java
@@ -1,51 +1,17 @@
 public class Roman {
     static String roman(int n) { 
-        if(n>=1000){
-            return "M" + roman(n-1000);
-        }
-        else if(n>=500){
-            if(n>=900){
-                return "CM" + roman(n-900);
-            }
-            else if(n>=800){
-                return "CCM" + roman(n-800);
-            }
-            else{
-                return "D" + roman(n-500);
+        int[] roman_vals = {1000, 900, 800, 500, 400, 100, 90, 80, 50, 40, 10, 9, 8, 5, 4, 1};
+        String[] roman_symbs = {"M", "CM", "CCM", "D", "CD", "C", "XC", "XXC", "L", "XL", "X", "IX", "IIX", "V", "IV", "I"};
+        for(int i=0; i<16; i++){
+            if(n >= roman_vals[i]){
+                return roman_symbs[i] + roman(n-roman_vals[i]);
             }
         }
-        else if(n>=100){
-            if(n>=400){
-                return "CD" + roman(n-400);
-            }
-            else{
-                return "C" + roman(n-100);
-            }
-        }
-        else if(n>=50){
-            if(n>=90){
-                return "XC" + roman(n-90);
-            }
-            else if(n>=80){
-                return "XXC" + roman(n-80);
-            }
-            else{
-                return "L" + roman(n-50);
-            }
-        }
-        else if(n>=10){
-            return "X" + roman(n-10);
-        }
-        else if(n>=5){
-            return "V" + roman(n-5);
-        }
-        else if(n>=1){
-            return "I" + roman(n-1);
-        }
-        else {
-            return "";
-        }
+        return "";
     }
+
+
+
     public static void main(String[] args) {
         try{
             int N = Integer.parseInt(args[0]);
